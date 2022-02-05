584917d83ee67870f5f80b460fbad8f6b975e91b
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-05 12:43:01 -0800

    commit tree d0d2ef5e2b2fd1a01cc6e4e63588fcb4117e4a60

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-function scuberGreetingForFeet(someValue){
-  // Write your code here!
-  if (someValue <=400) {
+// function scuberGreetingForFeet(someValue){
+//   // Write your code here!
+//   if (someValue <=400) {
+//     return 'This one is on me!'
+//   } else if (someValue > 2500) {
+//     return 'No can do.'
+//   } else if (someValue > 2000) {
+//     return 'I will gladly take your thirty bucks.'
+//   }
+//   }
+
+
+// function ternaryCheckCity(someCity){
+//   // Write your code here!
+//   return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+// }
+
+
+// function switchOnCharmFromTip(someTip){
+//   // Write your code here!
+//   if (someTip === 'generous') {
+//     return 'Thank you so much.'
+//   } else if (someTip === 'not as generous') {
+//     return 'Thank you.'
+//   } else {
+//     return 'Bye.'
+//   }
+
+// }
+
+
+function scuberGreetingForFeet(feetValue) {
+  if (feetValue <= 400){
     return 'This one is on me!'
-  } else if (someValue > 2500) {
-    return 'No can do.'
-  } else if (someValue > 2000) {
+  } else if (feetValue > 2000 && feetValue < 2500){
     return 'I will gladly take your thirty bucks.'
+  } else {
+    return "No can do."
   }
-  }
-
-
-function ternaryCheckCity(someCity){
-  // Write your code here!
-  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
+function ternaryCheckCity(city) {
+  return city==="NYC"?'Ok, sounds good.': 'No go.'
+}
 
-function switchOnCharmFromTip(someTip){
-  // Write your code here!
-  if (someTip === 'generous') {
-    return 'Thank you so much.'
-  } else if (someTip === 'not as generous') {
-    return 'Thank you.'
-  } else {
-    return 'Bye.'
+function switchOnCharmFromTip(tip) {
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+      case "not as generous":
+        return "Thank you."
+        break
+        default:
+          return "Bye."
   }
-
 }
